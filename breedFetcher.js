16043b1a21e4f5a8e48a94771e8704a32f2ee21f
George-was-here <george.burt@gmail.com>
2022-04-15 19:03:23 +0000

    Fixed error problem

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,31 @@
 const request = require('request');
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const breedName = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 
 
-const breedFetcher = (breed) => {
+const fetchBreedDescription = (breedName, callback) => {
   request(url, (error, response, body) => {
+    if (error) {
+      console.log(`Terribly sorry, but we seem to have encountered an error ${error}`);
+      return; 
+    }
     const data = JSON.parse(body);
     if (!error && data.length) {
       console.log(`Search query results: ${data[0].description}`);
-      console.log("Type of Data:", typeof data);
+      // console.log("Type of Data:", typeof data);
       return;
     }
     if (!error) {
       console.log(`You have not yet found what you are looking for... ${null} ${undefined}`);
       return;
     }
-    console.log(`Terribly sorry, but we seem to have encountered an error ${error}`);
-    return;
   });
 };
 
-breedFetcher(url);
+fetchBreedDescription();
 
+module.exports = { fetchBreedDescription };
 
 //EDGE CASES!!!
 //Write code to output an appropriate message if the requested breed is not found.
